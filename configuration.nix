ec52e51ee3ed244db5e8d903ea4d5a83d3fcbddd
_ORFLEM_ <zenkinzahar@gmail.com>
2025-10-14 01:18:23 +0700

    14.10.2025 - NixOS config update!
    Former-commit-id: 81d7df91b75d42b09995e7294bc752e0ba604293

--- configuration.nix
+++ configuration.nix
@@ -2,6 +2,22 @@
 {
   imports = [ ./hardware-configuration.nix ];
 
+  # ============================================================
+  # NIX SETTINGS
+  # ============================================================
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 20d";
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  # ============================================================
+  # BOOT
+  # ============================================================
   boot.loader = {
     grub = {
       enable = true;
@@ -12,44 +28,118 @@
     efi.canTouchEfiVariables = true;
   };
 
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 20d";
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # ============================================================
+  # CONSOLE & TTY (Kanagawa theme)
+  # ============================================================
+  console = {
+    font = "ter-v32n";
+    packages = with pkgs; [
+      terminus_font
+      spleen
+      unifont
+    ];
+    earlySetup = true;
+  };
+  
+  boot.kernelParams = [
+    "fbcon=font:ter-v32n"
+    "vt.default_utf8=1"
+    "consoleblank=0"
+    
+    # Kanagawa colors
+    "vt.default_red=22,195,118,192,126,149,106,200,114,232,152,230,127,147,122,220"
+    "vt.default_grn=22,64,148,163,156,127,149,192,115,36,187,195,180,138,168,215"
+    "vt.default_blu=29,67,106,110,216,184,137,147,105,36,108,132,202,169,159,186"
+  ];
+  
+  systemd.services.set-tty-colors = {
+    description = "Apply Kanagawa TTY colors and font";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "systemd-vconsole-setup.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    script = ''
+      ${pkgs.kbd}/bin/setfont ter-v32n
+      
+      for tty in /dev/tty{1..6}; do
+        ${pkgs.kbd}/bin/setfont -C "$tty" ter-v32n
+        echo -en "\e]P016161d" > "$tty"
+        echo -en "\e]P1c34043" > "$tty"
+        echo -en "\e]P276946a" > "$tty"
+        echo -en "\e]P3c0a36e" > "$tty"
+        echo -en "\e]P47e9cd8" > "$tty"
+        echo -en "\e]P5957fb8" > "$tty"
+        echo -en "\e]P66a9589" > "$tty"
+        echo -en "\e]P7c8c093" > "$tty"
+        echo -en "\e]P8727169" > "$tty"
+        echo -en "\e]P9e82424" > "$tty"
+        echo -en "\e]PA98bb6c" > "$tty"
+        echo -en "\e]PBe6c384" > "$tty"
+        echo -en "\e]PC7fb4ca" > "$tty"
+        echo -en "\e]PD938aa9" > "$tty"
+        echo -en "\e]PE7aa89f" > "$tty"
+        echo -en "\e]PFdcd7ba" > "$tty"
+      done
+    '';
   };
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # ============================================================
+  # NETWORK & LOCALIZATION
+  # ============================================================
   networking.hostName = "NixOS_PC";
   networking.networkmanager.enable = true;
-  time.timeZone = "Asia/Tomsk";
+  time.timeZone = "Europe/Amsterdam";
 
-  i18n.defaultLocale = "ru_RU.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "ru_RU.UTF-8";
-    LC_IDENTIFICATION = "ru_RU.UTF-8";
-    LC_MEASUREMENT = "ru_RU.UTF-8";
-    LC_MONETARY = "ru_RU.UTF-8";
-    LC_NAME = "ru_RU.UTF-8";
-    LC_NUMERIC = "ru_RU.UTF-8";
-    LC_PAPER = "ru_RU.UTF-8";
-    LC_TELEPHONE = "ru_RU.UTF-8";
-    LC_TIME = "ru_RU.UTF-8";
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
-  services.xserver.xkb = {
-    layout = "ru";
-    variant = "";
-  };
-
-  services.xserver.enable = true;
-  services.displayManager.sddm = {
+  # ============================================================
+  # DISPLAY & DESKTOP
+  # ============================================================
+  services.greetd = {
     enable = true;
-    wayland.enable = true;
-    theme = "astronaut-theme";
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
+        user = "greeter";
+      };
+    };
   };
-  services.displayManager.defaultSession = "hyprland";
-  programs.hyprland.enable = true;
+  
+  systemd.services.greetd.serviceConfig.ExecStartPre = [
+    "${pkgs.kbd}/bin/setfont ter-v32n"
+  ];
+  
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-hyprland
+      xdg-desktop-portal-gtk
+    ];
+  };
+
+  # ============================================================
+  # AUDIO & BLUETOOTH
+  # ============================================================
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -67,43 +157,6 @@
     };
   };
 
-  hardware.graphics.enable = true;
-  
-  # Это пример настроек пользователя | this is example for user settigns:
-  # users.users.<юзер | username> = {
-  #   isNormalUser = true;
-  #   description = "<ник | nikname>";
-  #   extraGroups = [ "networkmanager" "wheel" ];
-  #   packages = with pkgs; [];
-  # };
-
-  nixpkgs.config.allowUnfree = true;
-
-  fonts.packages = with pkgs; [
-    iosevka-bin
-    texlivePackages.cjk
-    texlivePackages.cjkutils
-    noto-fonts-cjk-sans
-    nerd-fonts._3270
-    nerd-fonts.mononoki
-  ];
-  fonts.fontconfig.enable = true;
-
-  services.dbus.enable = true;
-  services.tumbler.enable = true;
-  services.flatpak.enable = true;
-
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-hyprland
-      xdg-desktop-portal-gtk
-    ];
-  };
-
-  services.gvfs.enable = true;
-
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
@@ -115,34 +168,72 @@
   };
   services.blueman.enable = true;
 
+  # ============================================================
+  # GRAPHICS
+  # ============================================================
+  hardware.graphics.enable = true;
+
+  # ============================================================
+  # USER CONFIGURATION
+  # ============================================================
+  # Раскомментируй и настрой под себя:
+  # users.users.<username> = {
+  #   isNormalUser = true;
+  #   description = "<Your Name>";
+  #   extraGroups = [ "networkmanager" "wheel" ];
+  #   packages = with pkgs; [];
+  # };
+
+  # ============================================================
+  # SYSTEM SERVICES
+  # ============================================================
+  services.dbus.enable = true;
+  services.tumbler.enable = true;
+  services.flatpak.enable = true;
+  services.gvfs.enable = true;
+
   programs.dconf.enable = true;
-  services.dbus.packages = with pkgs; [ dconf gnome2.GConf gcr libnotify glib gtk3 ];
+  services.dbus.packages = with pkgs; [ dconf libnotify glib gtk3 ];
 
   services.gnome.gnome-keyring.enable = true;
   programs.seahorse.enable = true;
-  security.pam.services.sddm.enableGnomeKeyring = true;
 
+  # ============================================================
+  # FONTS
+  # ============================================================
+  fonts.packages = with pkgs; [
+    iosevka-bin
+    noto-fonts-cjk-sans
+    nerd-fonts._3270
+    nerd-fonts.mononoki
+  ];
+  fonts.fontconfig.enable = true;
+
+  # ============================================================
+  # PACKAGES
+  # ============================================================
   environment.systemPackages = with pkgs; [
-    # GUI
-    blueman
-    eww
-    file-roller
-    gnome-calculator
+    # === Hyprland Stack ===
     hyprland
     hyprlock
     hyprpaper
+    eww
+    rofi
+    greetd.tuigreet
+    
+    # === GUI Applications ===
+    blueman
+    file-roller
+    gnome-calculator
     kitty
     mpv
     mpvpaper
     pavucontrol
     pdfarranger
-    rofi
-    rose-pine-hyprcursor
     shotwell
-    (sddm-astronaut.override { embeddedTheme = "pixel_sakura"; })
     vscodium
 
-    # Console
+    # === CLI Tools ===
     btop
     browsh
     cava
@@ -165,7 +256,6 @@
     libnotify
     lsd
     micro
-    neofetch
     pamixer
     p7zip
     playerctl
@@ -184,7 +274,7 @@
     yazi
     zip
 
-    # AMD Card (Drivers)
+    # === AMD Graphics Stack ===
     amdvlk
     libdrm
     libGL
@@ -195,11 +285,9 @@
     mesa-demos
     vulkan-loader
     vulkan-validation-layers
-    virtualgl
-    virtualglLib
     wayland
 
-    # Thunar
+    # === File Manager (Thunar) ===
     ffmpegthumbnailer
     libgsf
     poppler
@@ -207,18 +295,21 @@
     xfce.thunar-archive-plugin
     xfce.tumbler
 
-    # GTK
+    # === Themes ===
     gnome-themes-extra
     gtk3
     (papirus-icon-theme.override { color = "yaru"; })
     gruvbox-gtk-theme
     kanagawa-gtk-theme
     rose-pine-cursor
+    rose-pine-hyprcursor
     libsForQt5.qt5ct
     qt6Packages.qt6ct
-    
   ];
 
+  # ============================================================
+  # ENVIRONMENT
+  # ============================================================
   environment.sessionVariables = {
     GTK_THEME = "Kanagawa-B";
     ICON_THEME = "Papirus-Dark";
@@ -233,79 +324,17 @@
   programs.nix-ld.libraries = with pkgs; [
     glib
     glibc
-    gobject-introspection
-    (python312.withPackages (ps: with ps; [
-      dbus-python
-      pygobject3
-      jedi-language-server
-    ]))
-    dbus
   ];
 
-  programs.hyprland = {
-    xwayland.enable = true;
-  };
-
-  programs.steam.enable = true;
-  programs.steam.package = pkgs.steam.override {
-    extraPkgs = pkgs: with pkgs; [ libdrm wayland ];
-  };
-
-  console = {
-      font = "ter-v32n";
-      packages = with pkgs; [
-       terminus_font
-       spleen
-       unifont
-       ];
-      earlySetup = true;  # Загружает шрифт в initrd
+  # ============================================================
+  # GAMING
+  # ============================================================
+  programs.steam = {
+    enable = true;
+    package = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [ libdrm wayland ];
     };
-    
-    # Оригинальные Kanagawa цвета на уровне ядра (применяются при загрузке)
-    boot.kernelParams = [
-      "fbcon=font:TER16x32"
-      "vt.default_utf8=1"
-      "consoleblank=0"
-      
-      # Оригинальные Kanagawa RGB цвета:
-      # 0=black, 1=red, 2=green, 3=yellow, 4=blue, 5=magenta, 6=cyan, 7=white
-      # 8=br_black, 9=br_red, 10=br_green, 11=br_yellow, 12=br_blue, 13=br_magenta, 14=br_cyan, 15=br_white
-      "vt.default_red=22,195,118,192,126,149,106,200,114,232,152,230,127,147,122,220"
-      "vt.default_grn=22,64,148,163,156,127,149,192,115,36,187,195,180,138,168,215"
-      "vt.default_blu=29,67,106,110,216,184,137,147,105,36,108,132,202,169,159,186"
-    ];
-    
-    # Systemd сервис для закрепления цветов после загрузки
-    systemd.services.set-tty-colors = {
-      description = "Apply Kanagawa TTY colors";
-      wantedBy = [ "multi-user.target" ];
-      after = [ "systemd-vconsole-setup.service" ];
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-      };
-      script = ''
-        for tty in /dev/tty{1..6}; do
-          # Оригинальные Kanagawa цвета
-          echo -en "\e]P016161d" > "$tty"  # black
-          echo -en "\e]P1c34043" > "$tty"  # red
-          echo -en "\e]P276946a" > "$tty"  # green
-          echo -en "\e]P3c0a36e" > "$tty"  # yellow
-          echo -en "\e]P47e9cd8" > "$tty"  # blue
-          echo -en "\e]P5957fb8" > "$tty"  # magenta
-          echo -en "\e]P66a9589" > "$tty"  # cyan
-          echo -en "\e]P7c8c093" > "$tty"  # white
-          echo -en "\e]P8727169" > "$tty"  # bright black
-          echo -en "\e]P9e82424" > "$tty"  # bright red
-          echo -en "\e]PA98bb6c" > "$tty"  # bright green
-          echo -en "\e]PBe6c384" > "$tty"  # bright yellow
-          echo -en "\e]PC7fb4ca" > "$tty"  # bright blue
-          echo -en "\e]PD938aa9" > "$tty"  # bright magenta
-          echo -en "\e]PE7aa89f" > "$tty"  # bright cyan
-          echo -en "\e]PFdcd7ba" > "$tty"  # bright white
-        done
-      '';
-    };
+  };
 
   system.stateVersion = "25.05";
 }
